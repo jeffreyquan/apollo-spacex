c5164a454cd61749798b6301d1fb54b56753ea35
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-06-26 15:30:53 +1000

    updated launches resolver to allow for pagination

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,7 +1,41 @@
+const { paginateResults } = require('./utils');
+
 module.exports = {
   Query: {
-    launches: (_,__, { dataSources }) => dataSources.launchAPI.getAllLaunches(),
+    launches: async (_, { pageSize = 20, after }, { dataSources }) => {
+      const allLaunches = await dataSources.launchAPI.getAllLaunches();
+      allLaunches.reverse();
+      const launches = paginateResults({
+        after,
+        pageSize,
+        results: allLaunches
+      });
+      return {
+        launches,
+        cursor: launches.length ? launches[launches.length - 1].cursor : null,
+        hasMore: launches.length ? launches[launches.length - 1].cursor !== allLaunches[allLaunches.length - 1].cursor : false
+      };
+    },
     launch: (_, { id } , { dataSources }) => dataSources.launchAPI.getLaunchById({ launchId: id }),
     me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser()
-  }
+  },
+  Mission: {
+    missionPatch: (mission, { size } = { size: 'LARGE' }) => {
+      return size == "SMALL" ? mission.missionPatchSmall : mission.missionPatchLarge
+    }
+  },
+  Launch: {
+    isBooked: async (launch, _, { dataSources }) => dataSources.userAPI.isBookedOnLauncch({ launchId: launch.id }),
+  },
+  User: {
+    trips: async (_, __, { dataSources }) => {
+      const launchIds = await dataSources.userAPI.getLaunchIdsByUser();
+      if (!launchIds.length) return [];
+      return (
+        dataSources.launchAPI.getLaunchByIds({
+          launchIds,
+        }) || []
+      );
+    },
+  },
 }
