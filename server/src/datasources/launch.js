e4cc4a0032294ce381d4eefcfa277d714dbb1f50
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-06-27 14:28:28 +1000

    fix typo from data to date

--- server/src/datasources/launch.js
+++ server/src/datasources/launch.js
@@ -14,7 +14,7 @@ class LaunchAPI extends RESTDataSource {
   launchReducer = launch => {
     return {
       id: launch.flight_number || 0,
-      cursor: `${launch.launch_data_unix}`,
+      cursor: `${launch.launch_date_unix}`,
       site: launch.launch_site && launch.launch_site.site_name,
       mission: {
         name: launch.mission_name,
