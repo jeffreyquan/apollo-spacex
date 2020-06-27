6877eff8c3d3455cc390713b02c827af1e539371
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-06-27 22:56:29 +1000

    fix typo in getLaunchesByIds function

--- server/src/datasources/launch.js
+++ server/src/datasources/launch.js
@@ -36,7 +36,7 @@ class LaunchAPI extends RESTDataSource {
 
   getLaunchesByIds = ({ launchIds }) => {
     return Promise.all(
-      launchIds.map(launchIds => this.getLaunchById({ launchId }))
+      launchIds.map(launchId => this.getLaunchById({ launchId }))
     );
   }
 }
