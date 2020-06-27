b4d9334fb14b509ce124ea166d91e31271c3aa29
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-06-27 22:35:36 +1000

    added comments to explain parts of the launches resolver

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -4,6 +4,7 @@ module.exports = {
   Query: {
     launches: async (_, { pageSize = 20, after }, { dataSources }) => {
       const allLaunches = await dataSources.launchAPI.getAllLaunches();
+      // we want these in reverse chronological order
       allLaunches.reverse();
       const launches = paginateResults({
         after,
@@ -13,7 +14,12 @@ module.exports = {
       return {
         launches,
         cursor: launches.length ? launches[launches.length - 1].cursor : null,
-        hasMore: launches.length ? launches[launches.length - 1].cursor !== allLaunches[allLaunches.length - 1].cursor : false
+        // if the cursor at the end of the paginated results is the same as the
+        // last item in _all_ results, then there are no more results after this
+        hasMore: launches.length
+          ? launches[launches.length - 1].cursor !==
+            allLaunches[allLaunches.length - 1].cursor
+          : false,
       };
     },
     launch: (_, { id } , { dataSources }) => dataSources.launchAPI.getLaunchById({ launchId: id }),
