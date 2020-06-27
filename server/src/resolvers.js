b653ebc9205ffc19b5c7a43e18ad43671fdb8033
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-06-27 22:44:42 +1000

    created login mutation

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -24,6 +24,12 @@ module.exports = {
     },
     launch: (_, { id } , { dataSources }) => dataSources.launchAPI.getLaunchById({ launchId: id }),
     me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser()
+  }, 
+  Mutation: {
+    login: async (_, { email }, { dataSources }) => {
+      const user = await dataSources.userAPI.findOrCreateUser({ email });
+      if (user) return Buffer.from(email).toString("base64");
+    }
   },
   Mission: {
     missionPatch: (mission, { size } = { size: 'LARGE' }) => {
