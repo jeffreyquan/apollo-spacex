842972525474ab8626f1639115be6dd7cade850c
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-06-29 21:56:45 +1000

    connect to apollo studio

--- server/src/index.js
+++ server/src/index.js
@@ -12,22 +12,25 @@ const store = createStore();
 const server = new ApolloServer({
   context: async ({ req }) => {
     // simple auth check on every request
-    const auth = req.headers && req.headers.authorization || "";
+    const auth = (req.headers && req.headers.authorization) || "";
     const email = Buffer.from(auth, "base64").toString("ascii");
     if (!isEmail.validate(email)) return { user: null };
     // find a user by their email
-    const users = await store.users.findOrCreate({ where: { email }});
-    const user = users && users[0] || null;
-    return { user: { ...user.dataValues }};
-  },  
+    const users = await store.users.findOrCreate({ where: { email } });
+    const user = (users && users[0]) || null;
+    return { user: { ...user.dataValues } };
+  },
   typeDefs,
   resolvers,
   dataSources: () => ({
     launchAPI: new LaunchAPI(),
     userAPI: new UserAPI({
-      store
-    })
-  })
+      store,
+    }),
+  }),
+  engine: {
+    reportSchema: true,
+  },
 });
 
 server.listen().then(({ url }) => {
