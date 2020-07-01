cd6c8cf7b043c47de1c61f6166c4681d20bc3230
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-07-01 14:58:50 +1000

    refactor get launches query to use launch fragment

--- client/src/pages/launches.tsx
+++ client/src/pages/launches.tsx
@@ -6,25 +6,32 @@ import { LaunchTile, Header, Button, Loading } from '../components';
 import { RouteComponentProps } from '@reach/router';
 import * as GetLaunchListTypes from "./__generated__/GetLaunchList";
 
+export const LAUNCH_TILE_DATA = gql`
+  fragment LaunchTile on Launch {
+    id
+    isBooked
+    rocket {
+      id
+      name
+    }
+    mission {
+      name
+      missionPatch
+    }
+  }
+`;
+
 const GET_LAUNCHES = gql`
   query launchList($after: String) {
     launches(after: $after) {
       cursor
       hasMore
       launches {
-        id
-        isBooked
-        rocket {
-          id
-          name
-        }
-        mission {
-          name
-          missionPatch
-        }
+        ...LaunchTile
       }
     }
   }
+  ${LAUNCH_TILE_DATA}
 `;
 
 interface LaunchesProps extends RouteComponentProps {}
