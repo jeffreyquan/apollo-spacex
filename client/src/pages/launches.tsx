0ba17c6deef90191e676098ef926e5847b05b1f4
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-07-01 14:04:06 +1000

    conditional rendering of fetch more button if there are more launches to fetch (pagination)

--- client/src/pages/launches.tsx
+++ client/src/pages/launches.tsx
@@ -30,7 +30,7 @@ const GET_LAUNCHES = gql`
 interface LaunchesProps extends RouteComponentProps {}
 
 const Launches: React.FC<LaunchesProps> = () => {
-  const { data, loading, error } = useQuery<GetLaunchListTypes.GetLaunchList, GetLaunchListTypes.GetLaunchListVariables>(GET_LAUNCHES);
+  const { data, loading, error, fetchMore } = useQuery<GetLaunchListTypes.GetLaunchList, GetLaunchListTypes.GetLaunchListVariables>(GET_LAUNCHES);
 
   if (loading) return <Loading />;
   if (error) return <p>ERROR</p>;
@@ -42,6 +42,30 @@ const Launches: React.FC<LaunchesProps> = () => {
       {data.launches && data.launches.launches && data.launches.launches.map((launch: any) => (
         <LaunchTile key={launch.id} launch={launch} />
       ))}
+      {data.launches && data.launches.hasMore && (
+        <Button
+          onClick={() => fetchMore({
+            variables: {
+              after: data.launches.cursor,
+            },
+            updateQuery: (prev, { fetchMoreResult, ...rest }) => {
+              if (!fetchMoreResult) return prev;
+              return {
+                ...fetchMoreResult,
+                launches: {
+                  ...fetchMoreResult.launches,
+                  launches: [
+                    ...prev.launches.launches,
+                    ...fetchMoreResult.launches.launches,
+                  ],
+                },
+              };
+            },
+          })}
+          >
+            Load More
+        </Button>)
+      }
     </Fragment>
   )
 }
