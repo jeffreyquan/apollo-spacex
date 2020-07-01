9073e036b1b74c75f72c8eee29ce00bdf9ca5e2a
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-07-01 13:50:28 +1000

    added get launches query in launches page

--- client/src/pages/launches.tsx
+++ client/src/pages/launches.tsx
@@ -1,14 +1,49 @@
 import React, { Fragment }  from 'react';
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+
+import { LaunchTile, Header, Button, Loading } from '../components';
 import { RouteComponentProps } from '@reach/router';
-import gql from 'graphql-tag';
+import * as GetLaunchListTypes from "./__generated__/GetLaunchList";
+
+const GET_LAUNCHES = gql`
+  query launchList($after: String) {
+    launches(after: $after) {
+      cursor
+      hasMore
+      launches {
+        id
+        isBooked
+        rocket {
+          id
+          name
+        }
+        mission {
+          name
+          missionPatch
+        }
+      }
+    }
+  }
+`;
 
 interface LaunchesProps extends RouteComponentProps {}
 
 const Launches: React.FC<LaunchesProps> = () => {
-  return <div />;
-}
+  const { data, loading, error } = useQuery<GetLaunchListTypes.GetLaunchList, GetLaunchListTypes.GetLaunchListVariables>(GET_LAUNCHES);
 
-export const LAUNCH_TILE_DATA = gql`
-`;
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR</p>;
+  if (!data) return <p>Not found</p>;
+
+  return (
+    <Fragment>
+      <Header />
+      {data.launches && data.launches.launches && data.launches.launches.map((launch: any) => (
+        <LaunchTile key={launch.id} launch={launch} />
+      ))}
+    </Fragment>
+  )
+}
 
 export default Launches;
