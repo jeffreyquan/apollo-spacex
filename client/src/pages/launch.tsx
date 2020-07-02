7fc2ad18c6171ca229718f3151efcc22b7683116
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-07-02 16:38:25 +1000

    added isInCart virtual field in launch query

--- client/src/pages/launch.tsx
+++ client/src/pages/launch.tsx
@@ -11,7 +11,7 @@ import { LAUNCH_TILE_DATA } from "./launches";
 export const GET_LAUNCH_DETAILS = gql`
   query LaunchDetails($launchId: ID!) {
     launch(id: $launchId) {
-      id
+      isInCart @client
       site
       isBooked
       rocket {
