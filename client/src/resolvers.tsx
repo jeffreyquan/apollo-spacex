f6dd35812e349343025f36bbd09bd78043564c2d
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-07-02 16:41:19 +1000

    define client schema

--- client/src/resolvers.tsx
+++ client/src/resolvers.tsx
@@ -1,1 +1,55 @@
-export default {}
+import gql from "graphql-tag";
+import { ApolloCache } from "apollo-cache";
+import { Resolvers } from "apollo-client";
+import * as GetCartItemTypes from "./pages/__generated__/GetCartItems";
+import * as LaunchTileTypes from "./pages/__generated__/LaunchTile";
+import { GET_CART_ITEMS } from "./pages/cart";
+
+export const schema = gql`
+  extend type Launch {
+    isInCart: Boolean!
+  }
+`;
+
+export const typeDefs = gql`
+  extend type Query {
+    isLogginedIn: Boolean!
+    cartItems: [ID!]!
+  }
+
+  extend type Launch {
+    isInCart: Boolean!
+  }
+
+  extend type Mutation {
+    addOrRemoveFromCart(id: ID!): [ID!]!
+  }
+`;
+
+type ResolverFn = (
+  parent: any,
+  args: any,
+  { cache } : { cache: ApolloCache<any> }
+) => any;
+
+interface ResolverMap {
+  [field: string]: ResolverFn;
+}
+
+interface AppResolvers extends Resolvers {
+  Launch: ResolverMap;
+}
+
+export const resolvers: AppResolvers = {
+  Launch: {
+    isInCart: (launch: LaunchTileTypes.LaunchTile, _, { cache }): boolean => {
+      const queryResult = cache.readQuery<GetCartItemTypes.GetCartItems>({
+        query: GET_CART_ITEMS
+      });
+      if (queryResult) {
+        return queryResult.cartItems.includes(launch.id);
+      }
+      return false;
+    }
+  }
+};
